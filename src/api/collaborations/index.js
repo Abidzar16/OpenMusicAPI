0521cf36729126332765708e5232f7b68f6fd67b
abidzarmgk <abidzarmgk@telkomuniversity.ac.id>
2021-07-10 19:20:10 +0700

    Final Change after lint

--- src/api/collaborations/index.js
+++ src/api/collaborations/index.js
@@ -5,9 +5,7 @@ module.exports = {
   name: 'collaborations',
   version: '1.0.0',
   register: async (server, { collaborationsService, playlistsService, validator }) => {
-    const collaborationsHandler = new CollaborationsHandler(
-      collaborationsService, playlistsService, validator,
-    );
+    const collaborationsHandler = new CollaborationsHandler(collaborationsService, playlistsService, validator,);
     server.route(routes(collaborationsHandler));
   },
 };
